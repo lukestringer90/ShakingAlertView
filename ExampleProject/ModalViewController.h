de181e21f408814102ad35c265d333ef04590ebd
Luke Stringer <luke.stringer@me.com>
2012-09-30 16:14:43 +0100

    Renamed project

---
+++ ExampleProject/ModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalViewController.h
+//  ShakingAlertView
+//
+//  Created by Luke on 21/09/2012.
+//  Copyright (c) 2012 Luke Stringer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+
+@end
---
+++ ExampleProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShakingAlertView
+//
+//  Created by Luke on 23/09/2012.
+//  Copyright (c) 2012 Luke Stringer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
