18affc4a941d4057a3f669f4861304f7c7c98a11
Luke Stringer <luke.stringer@me.com>
2013-02-18 01:11:35 +0000

    Tests for password entry.

--- ShakingAlertViewKiwiTest/ShakingAlertViewTests.m
+++ ShakingAlertViewKiwiTest/ShakingAlertViewTests.m
@@ -9,11 +9,18 @@
 #import "Kiwi.h"
 #import "ShakingAlertView.h"
 
+// Mirror the class extension in ShakingAlertView.m so we can get to the private parts of the class
+@interface ShakingAlertView ()
+@property (nonatomic, strong) UITextField *passwordField;
+- (void)animateIncorrectPassword;
+@end
+
+
 SPEC_BEGIN(ShakingAlertViewTests)
 
 describe(@"ShakingAlertView", ^{
     
-    context(@"Seting up", ^{
+    context(@"being set up", ^{
         
         __block ShakingAlertView *shakingAlertView = nil;
         __block NSString *title = @"Test Title";
@@ -21,7 +28,7 @@ describe(@"ShakingAlertView", ^{
         __block void (^correctPassword)(void) = ^(void) { NSLog(@"Correct Password"); };
         __block void (^dismissed)(void) = ^(void) { NSLog(@"Dimssed"); };
         
-        // Cleanup
+        // Cleaup
         afterEach(^{
             [shakingAlertView release];
             shakingAlertView = nil;
@@ -33,7 +40,7 @@ describe(@"ShakingAlertView", ^{
             [shakingAlertView shouldNotBeNil];
         });
         
-        it(@"should not be nil for plaitext password with specified blocks", ^{
+        it(@"should not be nil for plaintext password with specified blocks", ^{
             shakingAlertView = [[ShakingAlertView alloc] initWithAlertTitle:title
                                                            checkForPassword:password
                                                           onCorrectPassword:correctPassword
@@ -43,12 +50,59 @@ describe(@"ShakingAlertView", ^{
         
     });
     
-    context(@"Correct Password Entry", ^{
-        //
-    });
-    
-    context(@"Incorrect Passowrd Entry", ^{
-        //
+    context(@"received a password", ^{
+        __block ShakingAlertView *shakingAlertView = nil;
+        __block NSString *title = @"Test Title";
+        __block NSString *password = @"password";
+        
+        // Cleaup
+        afterEach(^{
+            [shakingAlertView release];
+            shakingAlertView = nil;
+        });
+        
+        it(@"should call success block for correct password entry", ^{
+            __block BOOL successReached;
+            
+            shakingAlertView = [[ShakingAlertView alloc] initWithAlertTitle:title
+                                                           checkForPassword:password
+                                                          onCorrectPassword:^{
+                                                              successReached = YES;
+                                                          }
+                                                 onDismissalWithoutPassword:nil];
+            // Show it
+            [shakingAlertView show];
+            
+            // Type password
+            shakingAlertView.passwordField.text = @"password";
+            
+            // Done with typing
+            [shakingAlertView textFieldShouldReturn:shakingAlertView.passwordField];
+            
+            // Block should be called
+            [[theValue(successReached) should] beTrue];
+            
+        });
+        
+        it(@"should shake for incorrect password entry", ^{
+            
+            shakingAlertView = [[ShakingAlertView alloc] initWithAlertTitle:title
+                                                           checkForPassword:password];
+            // Show it
+            [shakingAlertView show];
+            
+            // Type incorrect password
+            shakingAlertView.passwordField.text = @"incorrect_password";
+            
+            // Should shake when done typing
+            [[shakingAlertView should] receive:@selector(animateIncorrectPassword)];
+            
+            // Done with typing
+            [shakingAlertView textFieldShouldReturn:shakingAlertView.passwordField];
+            
+            
+        });
+        
     });
     
 });
